4ff9e4437911ad1288e3ad116745d75fa4004b61
hassanrazak <76017055+hassanrazak@users.noreply.github.com>
2025-08-25 11:19:59 -0400

    renamed db connection folder structure and updated import in entry point

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 require('dotenv').config()
 const app = require("./src/app")
 const port = process.env.PORT || 30001
-const dbConnection = require("./src/config.js/db")
+const dbConnection = require("./src/config/connection.js")
 
 dbConnection().then(() => {
     app.listen(port, () => {
